4cd7d80726bbfdcc38283b31e07eebb2934ba8dd
BrandonButler123 <85582593+BrandonButler123@users.noreply.github.com>
2021-09-23 18:17:53 -0230

    Update tutorial.js
    using array's

--- Tutorials/tutorial.js
+++ Tutorials/tutorial.js
@@ -44,3 +44,21 @@ var jedi = {
 
     }
 };
+
+
+var helloFrom = function 
+    (personName) {
+        return "Hello from " + personName;
+    }
+
+var people = ['Tom', 'Yoda', 'Ron'];
+
+people.push('Bob');
+people.push('Dr Evil');
+
+people.pop();
+
+for (var i = 0; i < people.length; i++) {
+    var greeting = helloFrom(people[i]);
+    alert(greeting);
+}
