dd03e6787e6f14c9f5b5f3ac5dccf3d564981f5a
BrandonButler123 <85582593+BrandonButler123@users.noreply.github.com>
2021-09-23 16:32:14 -0230

    Update tutorial.js
    logging to the console

--- Tutorials/tutorial.js
+++ Tutorials/tutorial.js
@@ -1,4 +1,8 @@
-var surname = prompt ('Greetings friend, may I enquire as to your surname?');
 
 var name = "Tom";
 var age = 20;
+
+var apples = 5, pears = 10;
+var peicesOfFruit = apples + pears;
+
+console.log(peicesOfFruit);
